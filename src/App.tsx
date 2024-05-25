fa940e5ab388c1edcbc38d70d1d25eb000f69ea9
whyKirbyy <timschmid02@icloud.com>
2024-05-25 17:14:32 +0200

    added sitting bunny after talking

--- src/App.tsx
+++ src/App.tsx
@@ -3,53 +3,65 @@ import MainTitel from './components/MainTitel';
 import StarBackground from './components/StarBackground';
 import talkingGif from './assets/whykirbyy-talking-animation-bubble.gif';
 import bunny from './assets/whykirbyy-bunny.png';
+import sittingBunny from './assets/sitting.png';
 import TextScroll from './components/TextScroll';
 import './App.css';
 
 function App() {
   const [showBunny, setShowBunny] = useState(true);
+  const [showGif, setShowGif] = useState(false);
+  const [showBunnySitting, setShowBunnySitting] = useState(false);
   const messages = [
     "Hi there!",
     "",
-    "Welcome the site of whyKirbyy!",
-    "here you can find all the things",
-    "he worked on and is working on",
-    "I hope you enjoy it!"
+    "Welcome to the site",
+    "of whyKirbyy!",
+    "here you can",
+    "find all the things",
+    "he worked on",
+    "and is working on",
+    "Enjoy!"
   ];
 
   useEffect(() => {
     const gifImage = new Image();
     gifImage.src = talkingGif;
 
-    const showBunnyTimer = setTimeout(() => {
+    const showGifTimer = setTimeout(() => {
+      setShowGif(true);
       setShowBunny(false);
     }, 2000);
 
-    const showTalkingBunnyTimer = setTimeout(() => {
+    const showGifBunnyTimer = setTimeout(() => {
+      setShowGif(false);
       setShowBunny(true);
-    }, 10800);
+    }, 10400);
+
+    const showSittingBunnyTimer = setTimeout(() => {
+      setShowBunny(false);
+      setShowBunnySitting(true);
+    }, 11500);
 
     return () => {
-      clearTimeout(showBunnyTimer);
-      clearTimeout(showTalkingBunnyTimer);
+      clearTimeout(showGifTimer);
+      clearTimeout(showGifBunnyTimer);
+      clearTimeout(showSittingBunnyTimer);
     };
   }, []);
 
   return (
     <div className="page-main-container">
-      <div className={"titel-page-main-container"}>
+      <div className="titel-page-main-container">
         <div className="titel-page-main">
           <MainTitel text="whyKirbyy"/>
         </div>
       </div>
-      <div className={"text-container"}>
-        {!showBunny && <TextScroll messages={messages}/>}
+      <div className="text-container">
+        {showGif && <TextScroll messages={messages}/>}
       </div>
-      {showBunny ? (
-        <img className="bunny" src={bunny} alt="whyKirbyy"/>
-      ) : (
-        <img className="gif" src={talkingGif} alt="whyKirbyy talking"/>
-      )}
+      {showBunny && <img className="bunny" src={bunny} alt="whyKirbyy"/>}
+      {showGif && <img className="gif" src={talkingGif} alt="whyKirbyy talking"/>}
+      {showBunnySitting && <img className="sitting-bunny" src={sittingBunny} alt="whyKirbyy sitting"/>}
       <StarBackground/>
     </div>
   );
