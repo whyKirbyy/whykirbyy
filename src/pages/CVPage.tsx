9c65a05cfea8c79349739dfcc5832089f02dbc02
whyKirbyy <timschmid02@icloud.com>
2024-05-28 17:34:46 +0200

    Stuff on the CV window

--- src/pages/CVPage.tsx
+++ src/pages/CVPage.tsx
@@ -1,5 +1,9 @@
 //import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import "../style/CVPage.scss";
+import Window from"../components/Window"
+import StarBackground  from "../components/StarBackground.tsx";
+
 
 const CVPage = () => {
   const navigate = useNavigate();
@@ -7,7 +11,9 @@ const CVPage = () => {
   return (
     <div>
       <h1>This is the CV Page</h1>
+      <Window />
       <button onClick={() => navigate('/')}>Go Back to Home</button>
+      <StarBackground />
     </div>
   );
 };
