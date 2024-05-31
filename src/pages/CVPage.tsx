ff904270964627c1d0b20f9b967cc502add0b155
whyKirbyy <timschmid02@icloud.com>
2024-05-31 09:36:59 +0200

    closes #21

--- src/pages/CVPage.tsx
+++ src/pages/CVPage.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import "../style/pages/CVPage.scss";
 import Window from "../components/Window";
 import StarBackground from "../components/StarBackground";
+import bunnyDesk from "../assets/bunnyDesk.png";
 
 const CVPage: React.FC = () => {
   const navigate = useNavigate();
@@ -14,16 +15,19 @@ const CVPage: React.FC = () => {
 
 
   useEffect(() => {
-    fetch('/src/components/CV.html')
+    fetch("/src/components/CV.html")
       .then(response => response.text())
       .then(data => setCvContent(data));
   }, []);
 
   return (
     <div className="cv-page-container">
-      <Window title="CV of whyKirbyy">
-        <div dangerouslySetInnerHTML={{ __html: cvContent }} />
-      </Window>
+      <img className="bunnyDesk" src={bunnyDesk} alt="sitting bunny" />
+      <div className={"cvwindow-page"}>
+        <Window title="CV of whyKirbyy">
+          <div dangerouslySetInnerHTML={{ __html: cvContent }} />
+        </Window>
+      </div>
       <button onClick={navigateToHome}>Go Back to Home</button>
       <StarBackground />
     </div>
