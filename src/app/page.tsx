121830afc45927b196510f97be954971d92d4df6
whyKirbyy <timschmid02@icloud.com>
2024-12-10 21:07:11 +0100

    started the next section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,58 +7,71 @@ export default function Home() {
     return (
         <div className={styles.page}>
             <div className={styles.about}>
-                <div>
-                    <div className={styles.dictSection}>
+                <div className={styles.dictSection}>
+                    <span className={[styles.darkerFont, styles.decorators].join(" ")}>about_content: Dict[str, str] = {"{"}<br/><br/></span>
+                    <div className={styles.textSection}>
                         <span
-                            className={[styles.darkerFont, styles.decorators].join(" ")}>about_content: Dict[str, str] = {"{"}<br/><br/></span>
-                        <div className={styles.textSection}>
-                            <span
-                                className={[styles.darkerFont, styles.decorators].join(" ")}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"tim_schmid": """</span>
-                            <div>
-                                <div className={styles.textBlock}>
-                                <span className={styles.text}>
-                                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-                                    invidunt ut labore et dolore magna aliquyam erat,sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-                                    Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-                                    consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-                                    <span className={[styles.darkerFont].join(" ")}>""", <br/><br/></span>
-                                </span>
-                                </div>
+                            className={[styles.darkerFont, styles.decorators].join(" ")}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"tim_schmid": """</span>
+                        <div>
+                            <div className={styles.textBlock}>
+                            <span className={styles.text}>
+                                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+                                invidunt ut labore et dolore magna aliquyam erat,sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+                                Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+                                consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                                <span className={[styles.darkerFont].join(" ")}>""", <br/><br/></span>
+                            </span>
                             </div>
                         </div>
                     </div>
-                    <div className={styles.dictSection}>
-                        <div className={styles.textSection}>
-                            <span
-                                className={[styles.darkerFont, styles.decorators].join(" ")}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"tech_stack":&nbsp;</span>
-                            <div>
-                                <div className={styles.textBlock}>
-                                <span className={styles.text}>
-                                    []
-                                    <span className={[styles.darkerFont].join(" ")}>, <br/><br/></span>
-                                </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={styles.dictSection}>
-                        <div className={styles.textSection}>
-                            <span
-                                className={[styles.darkerFont, styles.decorators].join(" ")}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"links":&nbsp;</span>
-                            <div>
-                                <div className={styles.textBlock}>
-                                <span className={styles.text}>
-                                    {"{"}{"}"}
-                                    <span className={[styles.darkerFont].join(" ")}>, <br/><br/></span>
-                                </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <span className={[styles.darkerFont, styles.decorators].join(" ")}>{"}"}</span>
                 </div>
+                <div className={styles.dictSection}>
+                    <div className={styles.textSection}>
+                        <span
+                            className={[styles.darkerFont, styles.decorators].join(" ")}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"tech_stack":&nbsp;</span>
+                        <div>
+                            <div className={styles.textBlock}>
+                            <span className={styles.text}>
+                                []
+                                <span className={[styles.darkerFont].join(" ")}>, <br/><br/></span>
+                            </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.dictSection}>
+                    <div className={styles.textSection}>
+                        <span
+                            className={[styles.darkerFont, styles.decorators].join(" ")}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"links":&nbsp;</span>
+                        <div>
+                            <div className={styles.textBlock}>
+                            <span className={styles.text}>
+                                {"{"}{"}"}
+                                <span className={[styles.darkerFont].join(" ")}>, <br/><br/></span>
+                            </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <span className={[styles.darkerFont, styles.decorators].join(" ")}>{"}"}</span>
             </div>
-            <div>
+            <div><br/><br/></div>
+            <div className={styles.projects}>
+                <span className={[styles.darkerFont, styles.decorators].join(" ")}>class Project:<br/><br/></span>
+                <span
+                    className={[styles.darkerFont, styles.decorators].join(" ")}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;def __init__(self, name: str, description: str, tech_stack: str, link: str, status: str) -{">"} None:<br/><br/></span>
+                <span className={`${styles.darkerFont} ${styles.decorators}`}>{Array(16).fill('\u00A0').join('')}self.name = name<br/><br/></span>
+                <span className={`${styles.darkerFont} ${styles.decorators}`}>{Array(16).fill('\u00A0').join('')}self.description = description<br/><br/></span>
+                <span className={`${styles.darkerFont} ${styles.decorators}`}>{Array(16).fill('\u00A0').join('')}self.tech_stack = tech_stack<br/><br/></span>
+                <span className={`${styles.darkerFont} ${styles.decorators}`}>{Array(16).fill('\u00A0').join('')}self.link = link<br/><br/></span>
+                <span className={`${styles.darkerFont} ${styles.decorators}`}>{Array(16).fill('\u00A0').join('')}self.status = status<br/><br/></span>
+                <span className={[styles.darkerFont, styles.decorators].join(" ")}>projects_content = [<br/><br/></span>
+                <span className={`${styles.darkerFont} ${styles.decorators}`}>{Array(8).fill('\u00A0').join('')}Project(<br/><br/></span>
+                <div className={styles.line}>
+                    <div className={`${styles.darkerFont} ${styles.decorators}`}>{Array(16).fill('\u00A0').join('')}name="</div>
+                    <div className={styles.text}>Portfolio Website</div>
+                    <div className={`${styles.darkerFont} ${styles.decorators}`}>",<br/><br/></div>
+                </div>
 
             </div>
             <BackToTopButton/>
