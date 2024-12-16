ce1a7923a5e56d6d779c49fad6f3e3bfce08741a
whyKirbyy <timschmid02@icloud.com>
2024-12-16 18:03:46 +0100

    commands to use are defined

--- src/app/components/terminalPopup.tsx
+++ src/app/components/terminalPopup.tsx
@@ -3,6 +3,7 @@ import styles from "../../styles/components/terminalPopup.module.css"
 
 const TerminalPopup: React.FC = () => {
     const inputRef = useRef<HTMLInputElement>(null);
+    const commands = ["copy", "pyhton3 resume.py", "language --go", "language --java", "language --typescript", "language --python"]
 
     useEffect(() => {
         if (inputRef.current) {
@@ -13,6 +14,9 @@ const TerminalPopup: React.FC = () => {
     return (
         <div className={styles.terminal}>
             <input type={"text"} name={"search"} id={"search"} className={styles.searchbar} placeholder={"Search for a command"} ref={inputRef}/>
+            <br></br>
+            <div><br/></div>
+            <div className={styles.suggested}>suggested</div>
         </div>
     );
 }
