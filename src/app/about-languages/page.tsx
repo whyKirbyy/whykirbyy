85c5e02dc3271f0d443b60270c78d9b278393a8d
whyKirbyy <timschmid02@icloud.com>
2024-12-28 21:04:18 +0100

    forgot to create a new branch

--- src/app/about-languages/page.tsx
+++ src/app/about-languages/page.tsx
@@ -1,9 +1,10 @@
 "use client"
 import React from "react";
+import styles from "./page.module.css"
 
 export default function Page() {
     return(
-        <div>
+        <div className={styles.page}>
             hi
         </div>
     );
